159a2a05ca29d024891a6a1d12d3788964e7788a
Santiago Buitrago <brayan.buitrago@escuelaing.edu.co>
2021-03-15 23:56:21 -0500

    Create, update, delete

--- src/main/resources/static/js/apiclient.js
+++ src/main/resources/static/js/apiclient.js
@@ -1,23 +1,50 @@
 apiclient = (function() {
     return {
-        getBlueprintsByAuthor: function(author, callback) {
-            $.ajax({
+        getBlueprintsByAuthor: function(author) {
+                var getPromise=$.ajax({
                 dataType: "json",
                 url: "http://localhost:8080/blueprints/"+author,
                 success: function (data) {
-                    callback(data)
+                 callback(data)
                 }
             });
+        return getPromise;
         },
-        getBlueprintsByNameAndAuthor: function( name, author, callback) {
 
-            $.ajax({
-                dataType: "json",
-                url: "http://localhost:8080/blueprints/"+author+"/"+name,
-                success: function (data) {
-                    callback(data)
+        getBlueprintsByNameAndAuthor: function( name, author) {
+            var getPromise=$.ajax({
+            dataType: "json",
+            url: "http://localhost:8080/blueprints/"+author+"/"+name,
+            success: function (data) {
+            callback(data)
                 }
             });
+             return getPromise;
+                    },
+                    updateBlueprint:function (blueprint) {
+                        var putPromise=$.ajax({
+                            url:"http://localhost:8080/blueprints/"+blueprint.author+"/"+blueprint.name,
+                            type:'PUT',
+                            data:JSON.stringify(blueprint),
+                            contentType: "application/json",
+                        });
+                        return putPromise;
+                    },
+                    deleteBlueprint:function (author,name) {
+                        var deletePromise=$.ajax({
+                            url:"http://localhost:8080/blueprints/"+author+"/"+name,
+                            type:'DELETE',
+                        });
+                        return deletePromise;
+                    },
+                    createBlueprint:function (blueprint) {
+                        var postPromise=$.ajax({
+                            url:"http://localhost:8080/blueprints",
+                            type:'POST',
+                            data:JSON.stringify(blueprint),
+                            contentType: "application/json",
+                        })
+                        return postPromise;
         }
     };
 })();
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,7 +2,11 @@ var api = apiclient;
 var Module = (function () {
     var _author;
       var open = false;
-       var currentBlueprint;
+        var currentBlueprint={
+               author: null,
+                   name: null,
+                   points: []
+           };
         function _map(list) {
             return mapList = list.map(function (blueprint) {
                 return {bpname: blueprint.name, numberPoints: blueprint.points.length};
@@ -60,18 +64,33 @@ var Module = (function () {
     };
 
     function getBlueprintsAuthor(author) {
-        _setAuthorName(author);
         if (author == "" || author == null) {
             alert("Ingrese un valor correcto de nombre");
         } else {
-            $("#blueprintAuthor > h2").text(author + "'s blueprints: ");
-            api.getBlueprintsByAuthor(author, _table);
+               api.getBlueprintsByAuthor(author).then(
+               function(data){
+                $("#blueprintAuthor > h2").text(author + "'s blueprints: ");
+               }
+               _setAuthorName(author);
+               _table(data);
+               },function () {
+                    $("#blueprintAuthor").empty();
+               }
+               $("#tableBlueprints tbody").empty();
+               $("#sumBlueprint").empty();
+               clearCanvas();
+               console.log("Error al consultar planos")
+           })
         }
     };
     function getBlueprintsAuthorAndName(author, name) {
         _setAuthorName(author);
-         api.getBlueprintsByNameAndAuthor(name, author, _graficar);
-    };
+         api.getBlueprintsByNameAndAuthor(name, author).then(
+         function (data) {
+            _graficar(data);
+         }
+    );
+};
 
      function init() {
             var canvas = document.getElementById("myCanvas"),
@@ -93,7 +112,7 @@ var Module = (function () {
                 var ny = event.pageY - offset.top;
                 currentBlueprint.points.push({x:nx,y:ny});
                 redraw(currentBlueprint);
-                context.fillRect(nx, ny, 10, 10);
+
         }
 
         //Helper function to get correct page offset for the Pointer coords
@@ -110,9 +129,59 @@ var Module = (function () {
             } while (obj = obj.offsetParent);
             return {left: offsetLeft, top: offsetTop};
         }
+    function updateBlueprint() {
+            if(currentBlueprint.name != null && currentBlueprint.author != null){
+                api.updateBlueprint(currentBlueprint).then(
+                    function () {
+                        getBlueprintsAuthor(_author);
+                    }
+                )
+            }
+        }
+        function createBlueprint() {
+            open=true;
+            clearCanvas();
+            currentBlueprint.author = document.getElementById("nombreUsuario").value;
+            if(currentBlueprint.author == null| currentBlueprint.author==""){
+                alert('Debe escribir el nombre del autor');
+            }
+            else{
+                currentBlueprint.name = prompt('nombre del plano');
+                currentBlueprint.points = [];
+                _setAuthorName(currentBlueprint.author);
+                api.createBlueprint(currentBlueprint).then(function (){
+                    $("#canvasTitle").text("Current blueprint: " + currentBlueprint.name);
+                    getBlueprintsAuthor(_author);
+                });
+            }
 
+
+        }
+        function deleteBlueprint() {
+            if(currentBlueprint.name != null && currentBlueprint.author != null&&currentBlueprint.author==_author) {
+                api.deleteBlueprint(currentBlueprint.author,currentBlueprint.name).then(
+                    function () {
+                        getBlueprintsAuthor(_author);
+                        currentBlueprint.author=null;
+                        currentBlueprint.name=null;
+                        currentBlueprint.points=[];
+                        open=false;
+                        $("#canvasTitle").empty();
+                        clearCanvas();
+                    }
+                )
+            }
+        }
+        function clearCanvas() {
+            var myCanvas = document.getElementById("myCanvas");
+            var ctx = myCanvas.getContext("2d");
+            ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+    }
     return {
             getBlueprintsAuthor: getBlueprintsAuthor,
             getBlueprintsAuthorAndName: getBlueprintsAuthorAndName,
             init: init,
+             updateBlueprint:updateBlueprint,
+             deleteBlueprint:deleteBlueprint,
+             createBlueprint:createBlueprint,
 })();
